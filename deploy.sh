4429c10f8d50b4f38e7d3419428061f7d99b79cb
Şahin <sahin.ucar.su@gmail.com>
2025-02-04 14:23:09 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+if [ -f artisan ]; then
+    $FORGE_PHP artisan migrate --force
+fi
+
+npm ci
+npm run build
+
+php artisan optimize:clear
+php artisan optimize
+
+( flock -w 10 9 || exit 1
+    echo 'Restarting FPM...'; sudo -S service php8.2-fpm reload ) 9>/tmp/fpmlock
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,14 +15,14 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans bg-gray-50 text-black/50 dark:bg-purple-950 dark:text-white/80 antialiased">
-        <div class="flex flex-col items-center py-12 sm:pt-0 ">
+        <div class="flex flex-col items-center px-6 py-12 sm:pt-0 ">
             <div class="mt-12">
-                <a href="/" wire:navigate class="text-2xl font-bold">
+                <a href="/" wire:navigate class="text-2xl font-bold text-gray-900 dark:text-white/50">
                     Westpack Festspiele
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-7xl lg:max-w-4xl mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-7xl lg:max-w-4xl mt-6 px-6 py-4 text-gray-900 dark:text-white/50 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
