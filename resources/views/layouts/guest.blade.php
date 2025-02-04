bfb14de19c94dc43cfb3a7a2cf78c0e45f8ba90c
Şahin <sahin.ucar.su@gmail.com>
2025-02-05 00:33:48 +0100

    feat: welcome page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans bg-gray-50 text-black/50 dark:bg-purple-950 dark:text-white/80 antialiased">
-        <div class="flex flex-col items-center px-6 py-12 sm:pt-0 ">
-            <div class="mt-12">
-                <a href="/" wire:navigate class="text-2xl font-bold text-gray-900 dark:text-white/50">
-                    Westpack Festspiele
-                </a>
-            </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 
-            <div class="w-full sm:max-w-7xl lg:max-w-4xl mt-6 px-6 py-4 text-gray-900 dark:text-white/50 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased text-white/80">
+    <div class="overflow-hidden">
+        <div class="min-h-screen flex items-center justify-center bg-radial-[at_25%_25%] from-fuchsia-800 from-60% to-fuchsia-950">
+            <div class="relative isolate">
+                <div class="py-24 sm:py-32 lg:pb-40">
+                    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                        <div class="mx-auto max-w-2xl text-center">
+                            {{ $slot }}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,23 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Westpack Festspiele</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased font-sans">
-        <div class="bg-gray-50 text-black/50 dark:bg-purple-950 dark:text-white/80">
-            <div class="relative min-h-screen flex flex-col items-center justify-center">
-                <div class="relative w-full max-w-2xl px-6 lg:max-w-5xl">
-                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <div class="flex lg:justify-center lg:col-start-2">
-                            Welcome to the Westpack Festspiele!
-                        </div>
-                    </header>
-
-                    <main class="mt-6">
-                        <div class="grid gap-6 lg:grid-cols-1 lg:gap-8 text-center">
-                            <h2 class="text-4xl">Schön, dass du bei unserer Challenge mitmachen möchtest!</h2>
-
-                            <p>
-                                Wir sammeln hier alle Anmeldungen und benötigen ein paar grobe Fitness-Daten von dir.
-                                Nach dem die Anmeldung geschlossen ist, werden alle Teilnehmenden in Teams aufgeteilt, welche dann gegeneinander antreten.
-                            </p>
-
-                            <a href="{{ route('register') }}" class="px-6 py-3 mt-6 hover:underline">
-                                Jetzt anmelden
-                            </a>
-                        </div>
-                    </main>
-
-                    <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                        🤩
-                    </footer>
-                </div>
-            </div>
+<x-guest-layout>
+    <div class="mb-8 flex justify-center">
+        <div class="relative rounded-full px-3 py-1 text-sm/6 text-white/50">
+            Schön, dass du bei unserer Challenge mitmachen möchtest!
         </div>
-    </body>
-</html>
+    </div>
+    <h1 class="text-5xl font-semibold tracking-tight text-balance sm:text-7xl">
+        Westpack Festspiele
+    </h1>
+    <p class="mt-12 text-lg font-medium text-pretty sm:text-xl/8 text-white/75">
+        Wir sammeln hier alle Anmeldungen und benötigen ein paar grobe Fitness-Daten von dir.
+        Nach dem die Anmeldung geschlossen ist, werden alle Teilnehmenden in Teams aufgeteilt, welche dann gegeneinander
+        antreten.
+    </p>
+    <div class="mt-10 flex items-center justify-center gap-x-6">
+        <a href="{{ route('register') }}" class="flex gap-2 items-center text-sm/6 font-semibold bg-fuchsia-950 px-4 py-2 rounded-md hover:bg-fuchsia-900 hover:text-white">
+            Jetzt anmelden
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                <path fill-rule="evenodd" d="M2 8c0 .414.336.75.75.75h8.69l-1.22 1.22a.75.75 0 1 0 1.06 1.06l2.5-2.5a.75.75 0 0 0 0-1.06l-2.5-2.5a.75.75 0 1 0-1.06 1.06l1.22 1.22H2.75A.75.75 0 0 0 2 8Z" clip-rule="evenodd" />
+            </svg>
+        </a>
+    </div>
+</x-guest-layout>
