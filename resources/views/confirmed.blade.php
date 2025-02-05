edbb8048a7d728b7bc77dd86f85637349c12196c
Şahin <sahin.ucar.su@gmail.com>
2025-02-05 02:11:07 +0100

    feat: confirm page

--- resources/views/confirmed.blade.php
+++ resources/views/confirmed.blade.php
@@ -1,47 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Westpack Festspiele</title>
+@include('shared.head')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="antialiased font-sans">
-        <div class="bg-gray-50 text-black/50 dark:bg-purple-950 dark:text-white/80">
-            <div class="relative min-h-screen flex flex-col items-center justify-center">
-                <div class="relative w-full max-w-2xl px-6 lg:max-w-5xl">
-                    <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <div class="flex lg:justify-center lg:col-start-2">
-                            Welcome to the Westpack Festspiele!
+<body class="font-sans antialiased text-white/80">
+    <div
+        class="min-h-screen flex items-center justify-center bg-radial-[at_25%_25%] from-fuchsia-800 from-60% to-fuchsia-950">
+        <div class="relative isolate">
+            <div class="py-24 sm:py-32 lg:pb-40">
+                <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div class="mx-auto text-center">
+                        <div class="mb-8 flex justify-center">
+                            <div class="relative rounded-full px-3 py-1 text-sm/6 text-white/50">
+                                Welcome to the Westpack Festspiele!
+                            </div>
                         </div>
-                    </header>
-
-                    <main class="mt-6">
-                        <div class="grid gap-6 lg:grid-cols-1 lg:gap-8 text-center">
-                            <h2 class="text-4xl">Awesome! You're now registered.</h2>
-
-                            <p>
-                                Further information will follow shortly. Stay tuned!
-                            </p>
-                        </div>
-                    </main>
-
-                    <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                        🤩
-                        <!-- Authentication -->
-                        <a href="{{ route('logout') }}" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                            {{ __('Log Out') }}
-                        </a>
-                    </footer>
+                        <h1 class="text-5xl font-semibold tracking-tight text-balance sm:text-7xl">
+                            Awesome! You're now registered.
+                        </h1>
+                        <p class="mt-12 text-lg font-medium text-pretty sm:text-xl/8 text-white/75">
+                            Further information will follow shortly. Stay tuned!
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
