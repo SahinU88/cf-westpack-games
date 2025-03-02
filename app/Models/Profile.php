28dbcf4385a4c760a237087af14db0bb7683fd27
Shift <shift@laravelshift.com>
2025-03-02 23:18:00 +0000

    Adopt Laravel type hints

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,7 +11,7 @@ class Profile extends Model
     /** @use HasFactory<\Database\Factories\ProfileFactory> */
     use HasFactory;
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
