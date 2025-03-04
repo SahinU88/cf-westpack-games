86214c0b95a3b7dcded9c4ddf727faf65e2c9e57
Şahin <sahin.ucar.su@gmail.com>
2025-03-04 14:21:09 +0100

    cleanup

--- resources/views/components/scores/layout.blade.php
+++ resources/views/components/scores/layout.blade.php
@@ -2,8 +2,8 @@
     <div class="mr-10 w-full pb-4 md:w-[220px]">
         <flux:navlist>
             <flux:navlist.item :href="route('scores.open-wod-25.1')" wire:navigate>{{ __('Open WOD 25.1') }}</flux:navlist.item>
-            <flux:navlist.item href="#">{{ __('Open WOD 25.2') }}</flux:navlist.item>
-            <flux:navlist.item href="#">{{ __('Open WOD 25.3') }}</flux:navlist.item>
+            {{-- <flux:navlist.item href="#">{{ __('Open WOD 25.2') }}</flux:navlist.item>
+            <flux:navlist.item href="#">{{ __('Open WOD 25.3') }}</flux:navlist.item> --}}
             <flux:navlist.item :href="route('dashboard')" icon="arrow-uturn-left" wire:navigate>{{ __('Zurück zur Übersicht') }}</flux:navlist.item>
         </flux:navlist>
     </div>
