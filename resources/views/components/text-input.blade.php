3ae35ee4eb906e689ea70f3a35072781fb72b792
Şahin <sahin.ucar.su@gmail.com>
2025-03-04 10:00:35 +0100

    chore: update to laravel12 with new starter-kit

--- resources/views/components/text-input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 bg-gray-900 text-gray-300 focus:border-indigo-600 focus:ring-indigo-600 rounded-md shadow-xs']) }}>
