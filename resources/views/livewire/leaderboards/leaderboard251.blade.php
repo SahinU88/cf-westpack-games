384c15ea02a55c1baf08d1d8cee55e6e4fcde13b
Şahin <sahin.ucar.su@gmail.com>
2025-03-06 15:59:35 +0100

    add subheading

--- resources/views/livewire/leaderboards/leaderboard251.blade.php
+++ resources/views/livewire/leaderboards/leaderboard251.blade.php
@@ -4,19 +4,19 @@
     <x-leaderboards.layout>
         <flux:tab.group>
             <flux:tabs wire:model="tab">
-                <flux:tab name="all">All</flux:tab>
                 <flux:tab name="rx">Rx</flux:tab>
                 <flux:tab name="scaled">Scaled</flux:tab>
+                <flux:tab name="all">All</flux:tab>
             </flux:tabs>
 
-            <flux:tab.panel name="all">
-                <x-leaderboards.leaderboard-251-individual :rankings="$rankings" division="Combined" />
-            </flux:tab.panel>
             <flux:tab.panel name="rx">
                 <x-leaderboards.leaderboard-251-individual :rankings="$rankingsRx" division="Rx" />
             </flux:tab.panel>
             <flux:tab.panel name="scaled">
                 <x-leaderboards.leaderboard-251-individual :rankings="$rankingsScaled" division="Scaled" />
+            </flux:tab.panel>
+            <flux:tab.panel name="all">
+                <x-leaderboards.leaderboard-251-individual :rankings="$rankings" division="Combined (zählt nicht zur Wertung)" />
             </flux:tab.panel>
         </flux:tab.group>
     </x-leaderboards.layout>
